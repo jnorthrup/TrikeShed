8aa9485cbdf3e1e09438086a40d55e6947ef6d46
jim <northrup.james@gmail.com>
2022-10-02 02:07:36 +0800

    intellij native project

--- build.gradle.kts
+++ build.gradle.kts
@@ -3,29 +3,13 @@ plugins {
 }
 
 group = "org.bereft"
-version = "1.0-SNAPSHOT"
+version = "1.0"
 
 repositories {
     mavenCentral()
 }
 
 kotlin {
-    jvm {
-        compilations.all {
-            kotlinOptions.jvmTarget = "1.8"
-        }
-        withJava()
-        testRuns["test"].executionTask.configure {
-            useJUnitPlatform()
-        }
-    }
-    js(BOTH) {
-        browser {
-            commonWebpackConfig {
-                cssSupport.enabled = true
-            }
-        }
-    }
     val hostOs = System.getProperty("os.name")
     val isMingwX64 = hostOs.startsWith("Windows")
     val nativeTarget = when {
@@ -35,18 +19,14 @@ kotlin {
         else -> throw GradleException("Host OS is not supported in Kotlin/Native.")
     }
 
-    
-    sourceSets {
-        val commonMain by getting
-        val commonTest by getting {
-            dependencies {
-                implementation(kotlin("test"))
+    nativeTarget.apply {
+        binaries {
+            executable {
+                entryPoint = "main"
             }
         }
-        val jvmMain by getting
-        val jvmTest by getting
-        val jsMain by getting
-        val jsTest by getting
+    }
+    sourceSets {
         val nativeMain by getting
         val nativeTest by getting
     }
--- src/nativeMain/kotlin/Main.kt
+++ src/nativeMain/kotlin/Main.kt
@@ -1,13 +1,3 @@
-package borg.trikeshed.metal
-
-import kotlinx.cinterop.*
-import platform.posix.*
-
-fun main(args: Array<String>) {
-    val file: CPointer<FILE>? = fopen("test.txt", "w")
-    if (file != null) {
-        fputs("Hello, World!", file)
-        fclose(file)
-    }
+fun main() {
+    println("Hello, Kotlin/Native!")
 }
-
