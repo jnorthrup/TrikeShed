e828fc6fcd85652b69030cbd6c4a0e9bbda81920
jim <northrup.james@gmail.com>
2022-10-21 16:17:02 +0800

    wip

--- src/jvmTest/kotlin/borg/trikeshed/placeholder/nars/NarseseParserTest.kt
+++ src/jvmTest/kotlin/borg/trikeshed/placeholder/nars/NarseseParserTest.kt
@@ -1,20 +1,28 @@
 package borg.trikeshed.placeholder.nars
 
-import borg.trikeshed.lib.parser.simple.CharSeries
+import borg.trikeshed.lib.collections.s_
+ import borg.trikeshed.lib.parser.simple.CharSeries
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
+
 import kotlin.test.*
 
 class NarseseParserTest {
 
-    private lateinit var cs: CharSeries
+
+    // read src/jvmTest/resources/sample.nars into lines
+    val lines: List<String> = Path("src/jvmTest/resources/sample.nars").readLines()
 
     @Test
-    fun testWord()    {
+    fun testParser() {
 
-        val input: CharSeries    =CharSeries("1234567890")
+        //TEST LINE 1
+        val line1 = lines[0]
+        val line1Expected = "  \$.13 (Calorie-->CompositeUnitOfMeasure). %1.0;.45% {345: o}".trim()
 
-        val result = NarseseParser.word(input)
-
-        assertEquals("1234567890", result?.asString())
-
+        val fsm = FSM(s_[task]) // create a FSM for the task parser
+        val result = fsm(CharSeries(line1)) // parse the line
+        println()
     }
 }
