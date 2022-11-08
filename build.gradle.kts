92ba194c382425d8dc760c5508f409185cedccaa
jim <northrup.james@gmail.com>
2022-11-08 18:55:39 +0800

    publish to maven local

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,51 +1,5 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
-//val KOTLIN_COMPILER_ARGS = listOf(
-//    "-opt-in=kotlin.RequiresOptIn",
-//    "-opt-in=kotlin.Experimental",
-//
-//    // https://docs.gradle.org/nightly/userguide/configuration_cache.html#config_cache:not_yet_implemented:storing_lambdas
-//    "-Xsam-conversions=class",
-//
-//    // Emit JVM type annotations in bytecode
-//    "-Xemit-jvm-type-annotations",
-//
-//    // Enhance not null annotated type parameter's types to definitely not null types (@NotNull T => T & Any)
-//    "-Xenhance-type-parameter-types-to-def-not-null",
-//
-//    // strict (experimental; treat as other supported nullability annotations)
-//    "-Xjsr305=strict",
-//
-//    // When using the IR backend, run lowerings by file in N parallel threads. 0 means use a thread per processor core. Default value is 1
-//    "-Xbackend-threads=0",
-//
-//    // Enable strict mode for some improvements in the type enhancement for loaded Java types based on nullability annotations,including freshly supported reading of the type use annotations from class files. See KT"-45671 for more details",
-//    "-Xtype-enhancement-improvements-strict-mode",
-//
-//    // Use fast implementation on Jar FS. This may speed up compilation time, but currently it's an experimental mode
-//    "-Xuse-fast-jar-file-system",
-//
-//    // Enable experimental context receivers
-//    "-Xcontext-receivers",
-//
-//    // Enable additional compiler checks that might provide verbose diagnostic information for certain errors.
-//    "-Xextended-compiler-checks",
-//
-//    // Enable incremental compilation
-//    "-Xenable-incremental-compilation",
-//
-//    // Enable compatibility changes for generic type inference algorithm
-//    "-Xinference-compatibility",
-//
-//    // Support inferring type arguments based on only self upper bounds of the corresponding type parameters
-//    "-Xself-upper-bound-inference",
-//
-//    // Eliminate builder inference restrictions like allowance of returning type variables of a builder inference call
-//    "-Xunrestricted-builder-inference",
-//
-//    "-Xuse-k2"
-//)
-
 plugins {
     kotlin("multiplatform") version "1.7.20"
     id("org.jetbrains.intellij") version "1.9.+" apply false
@@ -63,9 +17,10 @@ plugins {
     // gradle versions update plugin
     id("com.github.ben-manes.versions") version "0.42.0"
 
+    `maven-publish`
 }
 
-group = "org.bereft"
+group= "org.bereft"
 version = "1.0"
 
 repositories {
@@ -174,7 +129,10 @@ kotlin {
             }
         }
     }
-//    tasks.withType<KotlinCompile>().configureEach {
-//    kotlinOptions.freeCompilerArgs += KOTLIN_COMPILER_ARGS
-//}
+
 }
+/**
+ * export a pom file for the project
+ *
+ * requires: id("maven-publish")
+ */
