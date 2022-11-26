45ae0ba4f852fffbfbda413b73b8b2080e28782c
jim <northrup.james@gmail.com>
2022-11-26 23:16:18 +0800

    normalized codec methods moved into IOMemento.kt

--- src/jvmMain/kotlin/borg/trikeshed/isam/meta/PlatformCodec.kt
+++ src/jvmMain/kotlin/borg/trikeshed/isam/meta/PlatformCodec.kt
@@ -1,17 +1,25 @@
 package borg.trikeshed.isam.meta
 
 import borg.trikeshed.isam.meta.IOMemento.*
+import borg.trikeshed.isam.meta.IOMemento.Companion.readBool
+import borg.trikeshed.isam.meta.IOMemento.Companion.readByte
 import borg.trikeshed.isam.meta.IOMemento.Companion.readByteArray
 import borg.trikeshed.isam.meta.IOMemento.Companion.readCharSeries
+import borg.trikeshed.isam.meta.IOMemento.Companion.readInstant
+import borg.trikeshed.isam.meta.IOMemento.Companion.readLocalDate
+import borg.trikeshed.isam.meta.IOMemento.Companion.readNothing
+import borg.trikeshed.isam.meta.IOMemento.Companion.readString
 import borg.trikeshed.isam.meta.IOMemento.Companion.writeByteArray
 import borg.trikeshed.isam.meta.IOMemento.Companion.writeCharSeries
+import borg.trikeshed.isam.meta.IOMemento.Companion.writeInstant
+import borg.trikeshed.isam.meta.IOMemento.Companion.writeLocalDate
+import borg.trikeshed.isam.meta.IOMemento.Companion.writeNothing
+import borg.trikeshed.isam.meta.IOMemento.Companion.writeString
 import kotlinx.datetime.Instant
 import kotlinx.datetime.LocalDate
 
 actual object PlatformCodec {
 
-    actual val readBool = { value: ByteArray -> value[0] == 1.toByte() }
-    actual val readByte = { value: ByteArray -> value[0] }
     actual val readShort = { value: ByteArray -> ((value[0].toInt() shl 8) or (value[1].toInt() and 0xff)).toShort() }
     actual val readInt =
         { value: ByteArray -> (value[0].toInt() shl 24) or (value[1].toInt() and 0xff shl 16) or (value[2].toInt() and 0xff shl 8) or (value[3].toInt() and 0xff) }
@@ -19,10 +27,6 @@ actual object PlatformCodec {
         { value: ByteArray -> (value[0].toLong() shl 56) or (value[1].toLong() and 0xff shl 48) or (value[2].toLong() and 0xff shl 40) or (value[3].toLong() and 0xff shl 32) or (value[4].toLong() and 0xff shl 24) or (value[5].toLong() and 0xff shl 16) or (value[6].toLong() and 0xff shl 8) or (value[7].toLong() and 0xff) }
     actual val readFloat = { value: ByteArray -> java.lang.Float.intBitsToFloat(readInt(value)) }
     actual val readDouble = { value: ByteArray -> java.lang.Double.longBitsToDouble(readLong(value)) }
-    actual val readInstant = { value: ByteArray -> Instant.fromEpochSeconds(readLong(value), 0) }
-    actual val readLocalDate = { value: ByteArray -> LocalDate.fromEpochDays(readLong(value).toInt()) }
-    actual val readString = { value: ByteArray -> value.decodeToString() }
-    actual val readNothing = { _: ByteArray -> null }
     actual val writeBool = { value: Any? -> ByteArray(1).apply { this[0] = if (value as Boolean) 1 else 0 } }
     actual val writeByte = { value: Any? -> ByteArray(1).apply { this[0] = value as Byte } }
     actual val writeShort = { value: Any? ->
@@ -53,20 +57,6 @@ actual object PlatformCodec {
     }
     actual val writeFloat = { value: Any? -> writeInt(java.lang.Float.floatToIntBits(value as Float)) }
     actual val writeDouble = { value: Any? -> writeLong(java.lang.Double.doubleToLongBits(value as Double)) }
-    actual val writeInstant = { value: Any? ->
-        ByteArray(12).apply {
-            writeLong((value as Instant).epochSeconds)
-            writeInt(value.nanosecondsOfSecond)
-        }
-    }
-    actual val writeLocalDate = { value: Any? ->
-        ByteArray(8).apply<ByteArray> {
-            writeLong((value as LocalDate).toEpochDays().toLong())
-        }
-    }
-    actual val writeString = { value: Any? -> (value as String).encodeToByteArray() }
-    actual val writeNothing = { _: Any? -> ByteArray(0) }
-
 
     actual fun createEncoder(type: IOMemento, size: Int): (Any?) -> ByteArray {
         // must use corresponding  networkOrderSetXXX functions to set the bytes in the ByteArray
