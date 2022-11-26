a4ee07f34f584960b7ca39334fab00150bfe52ea
jim <northrup.james@gmail.com>
2022-11-26 23:22:31 +0800

    normalized codec methods moved into IOMemento.kt

--- src/jvmMain/kotlin/borg/trikeshed/isam/meta/PlatformCodec.kt
+++ src/jvmMain/kotlin/borg/trikeshed/isam/meta/PlatformCodec.kt
@@ -15,19 +15,26 @@ import borg.trikeshed.isam.meta.IOMemento.Companion.writeInstant
 import borg.trikeshed.isam.meta.IOMemento.Companion.writeLocalDate
 import borg.trikeshed.isam.meta.IOMemento.Companion.writeNothing
 import borg.trikeshed.isam.meta.IOMemento.Companion.writeString
-import kotlinx.datetime.Instant
-import kotlinx.datetime.LocalDate
 
 actual object PlatformCodec {
 
-    actual val readShort = { value: ByteArray -> ((value[0].toInt() shl 8) or (value[1].toInt() and 0xff)).toShort() }
+    actual val readShort = { value: ByteArray -> ((value[0].toInt() shl 8) or (value[1].toInt()
+            and 0xff)).toShort() }
     actual val readInt =
-        { value: ByteArray -> (value[0].toInt() shl 24) or (value[1].toInt() and 0xff shl 16) or (value[2].toInt() and 0xff shl 8) or (value[3].toInt() and 0xff) }
+        { value: ByteArray -> (value[0].toInt() shl 24) or (value[1].toInt() and 0xff shl 16) or (value[2].toInt()
+                and 0xff shl 8) or (value[3].toInt() and 0xff) }
     actual val readLong =
-        { value: ByteArray -> (value[0].toLong() shl 56) or (value[1].toLong() and 0xff shl 48) or (value[2].toLong() and 0xff shl 40) or (value[3].toLong() and 0xff shl 32) or (value[4].toLong() and 0xff shl 24) or (value[5].toLong() and 0xff shl 16) or (value[6].toLong() and 0xff shl 8) or (value[7].toLong() and 0xff) }
+        { value: ByteArray ->
+            (value[0].toLong() shl 56) or (value[1].toLong() and 0xff shl 48) or (value[2].toLong()
+                    and 0xff shl 40) or (value[3].toLong() and 0xff shl 32) or (value[4].toLong() and 0xff shl 24) or
+                    (value[5].toLong() and 0xff shl 16) or (value[6].toLong() and 0xff shl 8) or (value[7].toLong()
+                    and 0xff)
+        }
     actual val readFloat = { value: ByteArray -> java.lang.Float.intBitsToFloat(readInt(value)) }
     actual val readDouble = { value: ByteArray -> java.lang.Double.longBitsToDouble(readLong(value)) }
-    actual val writeBool = { value: Any? -> ByteArray(1).apply { this[0] = if (value as Boolean) 1 else 0 } }
+    actual val writeBool = { value: Any? -> ByteArray(1).apply { this[0] = if (value as
+                Boolean) 1
+    else 0 } }
     actual val writeByte = { value: Any? -> ByteArray(1).apply { this[0] = value as Byte } }
     actual val writeShort = { value: Any? ->
         ByteArray(2).apply {
@@ -56,48 +63,45 @@ actual object PlatformCodec {
         }
     }
     actual val writeFloat = { value: Any? -> writeInt(java.lang.Float.floatToIntBits(value as Float)) }
-    actual val writeDouble = { value: Any? -> writeLong(java.lang.Double.doubleToLongBits(value as Double)) }
+    actual val writeDouble = { value: Any? -> writeLong(java.lang.Double.doubleToLongBits(value as
+            Double)) }
 
-    actual fun createEncoder(type: IOMemento, size: Int): (Any?) -> ByteArray {
-        // must use corresponding  networkOrderSetXXX functions to set the bytes in the ByteArray
-        return when (type) {
-            IoBoolean -> writeBool
-            IoByte -> writeByte
-            IoShort -> writeShort
-            IoInt -> writeInt
-            IoLong -> writeLong
-            IoFloat -> writeFloat
-            IoDouble -> writeDouble
-            IoString -> writeString
-            IoInstant -> writeInstant
-            IoLocalDate -> writeLocalDate
-            IoCharSeries -> writeCharSeries
-            IoByteArray -> writeByteArray
-            IoNothing -> writeNothing
-        }
+    actual fun createEncoder(type: IOMemento, size: Int): (Any?) -> ByteArray = when (type) {
+        IoBoolean -> writeBool
+        IoByte -> writeByte
+        IoShort -> writeShort
+        IoInt -> writeInt
+        IoLong -> writeLong
+        IoFloat -> writeFloat
+        IoDouble -> writeDouble
+        IoString -> writeString
+        IoInstant -> writeInstant
+        IoLocalDate -> writeLocalDate
+        IoCharSeries -> writeCharSeries
+        IoByteArray -> writeByteArray
+        IoNothing -> writeNothing
     }
 
     actual fun createDecoder(
         type: IOMemento,
         size: Int,
-    ): (ByteArray) -> Any? {
-        return when (type) {
-            // all values must be read and written in network endian order
-            // we must call the marshalling functions inside the NetworkOrder ByteArray extension functions to ensure this
+    ): (ByteArray) -> Any? = when (type) {
+        // all values must be read and written in network endian order
+        // we must call the marshalling functions inside the NetworkOrder ByteArray extension functions to
+        // ensure this
 
-            IoBoolean -> readBool
-            IoByte -> readByte
-            IoShort -> readShort
-            IoInt -> readInt
-            IoLong -> readLong
-            IoFloat -> readFloat
-            IoDouble -> readDouble
-            IoString -> readString
-            IoLocalDate -> readLocalDate
-            IoInstant -> readInstant
-            IoCharSeries -> readCharSeries
-            IoByteArray -> readByteArray
-            IoNothing -> readNothing
-        }
+        IoBoolean -> readBool
+        IoByte -> readByte
+        IoShort -> readShort
+        IoInt -> readInt
+        IoLong -> readLong
+        IoFloat -> readFloat
+        IoDouble -> readDouble
+        IoString -> readString
+        IoLocalDate -> readLocalDate
+        IoInstant -> readInstant
+        IoCharSeries -> readCharSeries
+        IoByteArray -> readByteArray
+        IoNothing -> readNothing
     }
 }
