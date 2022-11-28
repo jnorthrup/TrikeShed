65704d1f569b8338331895c1d60ddd7f99942c2b
jim <northrup.james@gmail.com>
2022-11-28 22:47:49 +0800

    collapse metafile Reader

--- src/commonMain/kotlin/borg/trikeshed/isam/IsamMetaFileReader.kt
+++ src/commonMain/kotlin/borg/trikeshed/isam/IsamMetaFileReader.kt
@@ -1,6 +1,8 @@
 package borg.trikeshed.isam
 
-
+import borg.trikeshed.isam.meta.IOMemento
+import borg.trikeshed.isam.meta.PlatformCodec
+import borg.trikeshed.lib.*
 /**
  * 1. create a class that can read the metadata file and create a collection of record constraints
  *
@@ -14,30 +16,72 @@ Open_time Close_time Open High Low Close Volume Quote_asset_volume Number_of_tra
 IoInstant IoInstant IoDouble IoDouble IoDouble IoDouble IoDouble IoDouble IoInt IoDouble IoDouble
 ```
 
- TODO: the name "Reader" was initial quick and dirty reader testing.  this classname and the metafile format
- should be renamed to something more appropriate*/
-expect class IsamMetaFileReader {
-    /**
-     * 1. open the metafile descriptor for reading
-    1.  mmap the file into memory
-    1. close the file descriptor
-    1.  parse the file into a collection of record constraints
-    1. update recordlen
-    1. track meta isam field constraints:  name, type, begin, end , decoder, encoder
-    1. sanity check begin and end against defined IOMemento networkSizes
-    1. log (DEBUG) some dimension features and statistics about the record layouts
-    1.  return the collection of record constraints
-     */
-    fun open()
-    override fun toString(): String
+the ebnf we can use is:
 
-    companion object {
-        /** metafile writer function
-         * 1. open the metafile descriptor for writing
-         * 1. write the file from a collection of record constraints
-         * 1. close the file descriptor
-         */
-        fun write(metafilename: String, recordMetas: List<RecordMeta>)
+```
+metafile :=  (coords WS names WS .. EOL)*
+coords :=  (coord WS)* coord
+coord :=  number
+names :=  (name WS)* name
+name :=  string
+TypeMemento :=  (IoType WS)* IoType
+IoType :=  IoInstant | IoDouble | IoString | IoInt
+```
+ * 2. create a class that can create the binary file
+ *
+ * the binary file format follows this sample
+ *
+ */
+ class IsamMetaFileReader(val metafileFilename: String) {
+
+    val recordlen: Int by lazy {
+        constraints.last().end
+    }
+    val constraints: Series<RecordMeta> by lazy {
+        open()
+        constraints1.toSeries()
+    }
+
+    private lateinit var constraints1: List<RecordMeta>
+     fun open(){
+        //use readBytes and decodeString to read the lines into
+//        val lines = buf.readBytes(size).decodeToString().lines().filterNot { it.trim().startsWith("#") }.map(String::trim)
+        val lines=Files.readAllLines(metafileFilename ).filterNot {it.trim().startsWith('#')  }
+        //split on \s+
+        val coords = lines[0].split("\\s+".toRegex())
+        val names = lines[1].split("\\s+".toRegex())
+        val types = lines[2].split("\\s+".toRegex())
+
+
+        this@IsamMetaFileReader.constraints1 = names.zip(types).mapIndexed { index, (name, type) ->
+            val begin = coords[2 * index].toInt()
+            val end = coords[2 * index + 1].toInt()
+            val ioMemento: IOMemento = IOMemento.valueOf(type)
+            //use PlatformCodec to get the decoder and encoder
+            val decoder = ioMemento.createDecoder(end - begin)
+            val encoder = ioMemento.createEncoder(end - begin)
+            RecordMeta(name, ioMemento, begin, end, decoder, encoder)
+        }
+    }
+
+    //toString
+     override fun toString(): String {
+        return "IsamMetaFileReader(metafileFilename='$metafileFilename', recordlen=$recordlen, constraints=$constraints)"
+    }
+    /** metafile writer function
+     * 1. open the metafile descriptor for writing
+     * 1. write the file from a collection of record constraints
+     * 1. close the file descriptor
+     */
+     companion object {
+         fun write(metafilename: String, recordMetas: List<RecordMeta>) {
+            val lines = mutableListOf<String>()
+            lines.add("# format:  coords WS .. EOL names WS .. EOL TypeMememento WS .. [EOL]")
+            lines.add("# last coord is the recordlen")
+            lines.add(recordMetas.joinToString(" ") { it.begin.toString() + " " + it.end })
+            lines.add(recordMetas.joinToString(" ") { it.name })
+            lines.add(recordMetas.joinToString(" ") { it.type.name })
+            Files.write(metafilename, lines)
+        }
     }
 }
- 
