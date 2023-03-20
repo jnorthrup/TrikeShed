a7843f7f691bdc204771573972d2cde82285a9bb
jim <northrup.james@gmail.com>
2023-03-21 03:05:11 +0800

    iterateLines buffer-copy-edition works as hoped.

--- src/jvmMain/kotlin/borg/trikeshed/common/Files.kt
+++ src/jvmMain/kotlin/borg/trikeshed/common/Files.kt
@@ -70,90 +70,89 @@ actual object Files {
         }
     }
 
+    /**probably leaks a file handle here*/
     actual fun iterateLines(fileName: String, bufsize: Int): Iterable<Join<Long, Series<Byte>>> {
 
         val file = File(fileName)
+        val input = file.inputStream()
+        val theIterable = object : Iterable<Join<Long, Series<Byte>>> {
+            var fileClosed = false
+            val accum: MutableList<ByteArray> = mutableListOf()
+            var curBuf: ByteArray? = null
+            var curlinepos = 0L
+            fun drainAccum(): ByteArray {
 
-        file.inputStream().use { input ->
-            val theIterable = object : Iterable<Join<Long, Series<Byte>>> {
-                override fun iterator(): Iterator<Join<Long, Series<Byte>>> {
-                    var fileClosed = false
-                    var accum: MutableList<ByteArray> = mutableListOf()
-                    var curBuf: ByteArray? = null
-                    var curlinepos = 0L
-                    fun drainAccum(): ByteArray {
+                val ret = ByteArray(accum.sumOf { it.size })
+                var offset = 0
+                for (a in accum) {
+                    a.copyInto(ret, offset)
+                    offset += a.size
+                }
+                accum.clear()
+                return ret
+            }
 
-                        val ret = ByteArray(accum.sumOf { it.size })
-                        var offset = 0
-                        for (a in accum) {
-                            a.copyInto(ret, offset)
-                            offset += a.size
+            fun newLine(): Join<Long, Series<Byte>>? {
+                do {
+                    if (fileClosed) return null
+                    if (curBuf == null) {
+                        curBuf = ByteArray(bufsize)
+                        val read = input.read(curBuf)
+                        if (read == -1) {
+                            fileClosed = true
+                            curBuf = null
                         }
-                        accum.clear()
-                        return ret
+                    }
+                    if (curBuf == null) {
+                        val drainAccum = drainAccum()
+                        if (drainAccum.isEmpty()) return null
+                        return curlinepos j drainAccum.toSeries()
                     }
 
-                    fun newLine(): Join<Long, Series<Byte>>? {
-                        do {
-                            if (fileClosed) return null
-                            if (curBuf == null) {
-                                curBuf = ByteArray(bufsize)
-                                val read = input.read(curBuf)
-                                if (read == -1) {
-                                    fileClosed = true
-                                    curBuf = null
-                                }
-                            }
-                            if (curBuf == null) {
-                                val drainAccum = drainAccum()
-                                if (drainAccum.isEmpty()) return null
-                                return curlinepos j drainAccum.toSeries()
-                            }
+                    //right here we should be at the split  point of the line. we either drain the accum and keep a curBuff
+                    //or accumulate the whole remaining buffer and null out curBuf and loop back until newline or eof
+                    var mark = 0
+                    ByteSeries(curBuf!!).let { bs ->
+                        if (bs.seekTo('\n'.code.toByte())) {
+                            val carry = bs.slice
+                            val terminus = bs.flip()
 
-                            //right here we should be at the split  point of the line. we either drain the accum and keep a curBuff
-                            //or accumulate the whole remaining buffer and null out curBuf and loop back until newline or eof
-                            var mark = 0
-                            ByteSeries(curBuf!!).let { bs ->
-                                if (bs.seekTo('\n'.code.toByte())) {
-                                    val carry = bs.slice
-                                    val terminus = bs.flip()
-
-                                    accum += terminus.run { ByteArray(rem, ::get) }
-                                    curBuf = carry.run { ByteArray(rem, ::get) }
-                                    val drainAccum = drainAccum()
-                                    val r = curlinepos j drainAccum.toSeries()
-                                    curlinepos += drainAccum.size
-                                    return r
-                                } else {
-                                    accum += curBuf!!
-                                    curBuf = null
-
-
-                                }
-                            }
-
-                        } while (true)
-
-
+                            accum += terminus.run { ByteArray(rem, ::get) }
+                            curBuf = carry.run { ByteArray(rem, ::get) }
+                            val drainAccum = drainAccum()
+                            val r = curlinepos j drainAccum.toSeries()
+                            curlinepos += drainAccum.size
+                            return r
+                        } else {
+                            accum += curBuf!!
+                            curBuf = null
+                        }
                     }
-                    return object : Iterator<Join<Long, Series<Byte>>> {
-                        var nextLine: Join<Long, Series<Byte>>? = null
-                        override fun hasNext(): Boolean {
-                            if (nextLine == null) nextLine = newLine()
-                            return nextLine != null
-                        }
 
-                        override fun next(): Join<Long, Series<Byte>> {
-                            if (nextLine == null) nextLine = newLine()
-                            val retval = nextLine!!
-                            nextLine = null
-                            return retval
-                        }
+                } while (true)
+
+
+            }
+
+            override fun iterator(): Iterator<Join<Long, Series<Byte>>> {
+
+                return object : Iterator<Join<Long, Series<Byte>>> {
+                    var nextLine: Join<Long, Series<Byte>>? = null
+                    override fun hasNext(): Boolean {
+                        if (nextLine == null) nextLine = newLine()
+                        return nextLine != null
+                    }
+
+                    override fun next(): Join<Long, Series<Byte>> {
+                        if (nextLine == null) nextLine = newLine()
+                        val retval = nextLine!!
+                        nextLine = null
+                        return retval
                     }
                 }
             }
-            return theIterable
         }
+        return theIterable
     }
 }
 
