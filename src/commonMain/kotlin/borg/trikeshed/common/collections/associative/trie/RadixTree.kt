928080cfa3a04163baa0e35751cfe5d46bf54bf0
jim <northrup.james@gmail.com>
2023-04-28 00:42:36 +0800

    almost-tested safety checkin whilst intellij craps out randomly

--- src/commonMain/kotlin/borg/trikeshed/common/collections/associative/trie/RadixTree.kt
+++ src/commonMain/kotlin/borg/trikeshed/common/collections/associative/trie/RadixTree.kt
@@ -1,8 +1,6 @@
 package borg.trikeshed.common.collections.associative.trie
 
-import borg.trikeshed.common.collections._l
 import borg.trikeshed.lib.*
-
 
 /**
  * a prefix tree (trie) implementation
@@ -12,6 +10,10 @@ class RadixTree<C : Comparable<C>>(var root: RadixTreeNode<C>? = null) {
         root = root?.plus(key) ?: RadixTreeNode(key, true)
         return root!!
     }
+
+    // Inside the RadixTree class
+    fun keys(lowerBound: Series<C>? = null, upperBound: Series<C>? = null): List<Series<C>> =
+        root?.allKeysFromNode(emptySeries(), lowerBound, upperBound) ?: emptyList()
 }
 
 /**
@@ -40,33 +42,22 @@ class RadixTreeNode<C : Comparable<C>>(
 
             // If the current node has children, we try to find a child with a matching prefix for the remaining key
             //using binarysearch to retain sorted order
-//            if (children != null)// for (child in children!!)
-//                if (child.key.first() == remainingKey.first())
-//                    return child + remainingKey
-//                else children = mutableListOf()
-//
-//            // If there is no matching child, we create a new child node with the remaining key and add it to the children
-//            val newNode = RadixTreeNode(remainingKey, true)
-//            children!!.add(newNode)
-//            return this
             children?.let { children ->
-                var index = children.binarySearch { it.key.first().compareTo(remainingKey.first()) }
-                //get the two nieghbors of the insertion point
-                index = if (index < 0) -index - 1 else index
-                _l[children.getOrNull(index - 1),
-                    children.getOrNull(index)
-                ].filterNotNull().maxByOrNull {//by length of common key prefix
-                    it.key.commonPrefixWith(remainingKey).size
-                }?.let { bestMatch ->
-                    val cps = bestMatch.key.commonPrefixWith(remainingKey).size
-                    if (cps > 0) {
-                        return bestMatch + remainingKey
-                    } else {
+                var index = children.binarySearchBy(remainingKey.take(1).cpb) { it.key.take(1).cpb }
+                when {
+                    index >= 0 -> return children[index] + remainingKey
+                    else -> {
+                        index = -index - 1
                         val newNode = RadixTreeNode(remainingKey, true)
                         children.add(index, newNode)
                         return this
                     }
                 }
+            } ?: run {
+                // If the current node has no children, we just create a new child node for the remaining key
+                val newNode = RadixTreeNode(remainingKey, true)
+                children = mutableListOf(newNode)
+                return this
             }
         }
 
@@ -87,13 +78,33 @@ class RadixTreeNode<C : Comparable<C>>(
             val newOtherNode = RadixTreeNode(remainingOtherKey, true)
 
             // Set the new internal node's children
-            newInternalNode.children = mutableListOf(newChildNode, newOtherNode)
-
+            newInternalNode.children =
+                (if (remainingCurrentKey.cpb < remainingOtherKey.cpb) mutableListOf(
+                    newChildNode,
+                    newOtherNode
+                ) else mutableListOf(newOtherNode, newChildNode))
             return newInternalNode
         }
 
         // If the common prefix length is 0, it means that there is no common prefix, so we cannot insert the other key
         throw IllegalArgumentException("Cannot insert a key with no common prefix.")
+    }
+
+    fun allKeysFromNode(prefix: Series<C>, lowerBound: Series<C>?, upperBound: Series<C>?): List<Series<C>> {
+        val currentPrefix = prefix + key
+        val keys = mutableListOf<Series<C>>()
+
+        if (term && (lowerBound == null || currentPrefix.cpb >= lowerBound.cpb) && (upperBound == null ||
+                    currentPrefix.cpb <= upperBound.cpb)
+        ) keys.add(currentPrefix)
+
+        children?.forEach { child ->
+            if ((lowerBound == null || child.key.cpb >= lowerBound.drop(prefix.size).cpb) &&
+                (upperBound == null || child.key.cpb <= upperBound.drop(prefix.size).cpb)
+            ) keys.addAll(child.allKeysFromNode(currentPrefix, lowerBound, upperBound))
+        }
+
+        return keys
     }
 
 }
--- src/commonTest/kotlin/borg/trikeshed/common/collections/associative/trie/RadixTreeTest.kt
+++ src/commonTest/kotlin/borg/trikeshed/common/collections/associative/trie/RadixTreeTest.kt
@@ -1,7 +1,6 @@
 package borg.trikeshed.common.collections.associative.trie
 
-import borg.trikeshed.lib.debug
-import borg.trikeshed.lib.toSeries
+import borg.trikeshed.lib.*
 import kotlin.test.Test
 
 class RadixTreeTest {
@@ -20,8 +19,23 @@ class RadixTreeTest {
         tree + banshee
         debug { }
         // Verify the nodes
+        tree.root?.let { root ->
+            assert(0 == root.key.cpb.compareTo("ban".toSeries().cpb))
+            assert(root.term)
+            assert(root.children?.size == 3)
+            root.children?.let { children ->
+                assert(children[0].key.asString() == "ana".toSeries().asString())
+                assert(children[0].term)
+                assert(children[0].children == null)
+                assert(children[1].key.asString() == "ner".toSeries().asString())
+                assert(children[1].term)
+                assert(children[1].children == null)
+                assert(children[2].key.asString() == "shee".toSeries().asString())
+                assert(children[2].term)
+                assert(children[2].children == null)
+            }
+        }
 
+        println(tree.keys())
     }
 }
-
-
