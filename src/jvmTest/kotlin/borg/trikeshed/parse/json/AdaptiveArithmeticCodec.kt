a434762586a8591677bc68b72b7d57df1d4789aa
jim <northrup.james@gmail.com>
2023-05-03 15:51:51 +0800

    wip

--- src/jvmTest/kotlin/borg/trikeshed/parse/json/AdaptiveArithmeticCodec.kt
+++ src/jvmTest/kotlin/borg/trikeshed/parse/json/AdaptiveArithmeticCodec.kt
@@ -20,15 +20,16 @@ class AdaptiveArithmeticCodec {
             upper = lower + rangeSize * cumulativeFrequency(state.toInt() + 1) - 1uL
             lower += rangeSize * cumulativeFrequency(state.toInt())
             updateFrequencies(state.toInt())
-            yield(lower)
         }
+        yield(lower)
     }
 
-    fun decode(input: Iterable<ULong>): Sequence<UByte> = sequence {
+    fun decode(input: Iterable<ULong>, length: Int): Sequence<UByte> = sequence {
+        val encodedValue = input.first()
         var lower: ULong = 0uL
         var upper: ULong = ULong.MAX_VALUE - 1uL
 
-        for (encodedValue in input) {
+        for (i in 0 until length) {
             val rangeSize: ULong = (upper - lower + 1uL) / totalFrequency.toULong()
 
             for (state in stateFrequencies.indices) {
@@ -49,7 +50,7 @@ fun main() {
     val codec = AdaptiveArithmeticCodec()
     val input: List<UByte> = listOf<UByte>(0u, 1u, 2u, 3u, 0u, 1u, 2u, 3u, 0u, 1u, 2u, 3u)
     val encoded: List<ULong> = codec.encode(input).toList()
-    val decoded: List<UByte> = codec.decode(encoded).toList()
+    val decoded: List<UByte> = codec.decode(encoded, input.size).toList()
     println("Input: $input")
     println("Encoded: $encoded")
     println("Decoded: $decoded")
