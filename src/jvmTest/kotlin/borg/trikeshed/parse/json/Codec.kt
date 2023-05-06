974ee369756ff07ee1083ab860fe8e8889c33d8c
jim <northrup.james@gmail.com>
2023-05-06 16:46:22 +0800

    Implement a frequency model for counting state occurrences: Create a class to represent the frequency model (e.g., class FrequencyModel). Inside the class, store a mutable map to keep track of the frequency of each character. Add a method to update the frequency table based on the input.

--- src/jvmTest/kotlin/borg/trikeshed/parse/json/Codec.kt
+++ src/jvmTest/kotlin/borg/trikeshed/parse/json/Codec.kt
@@ -1,30 +1,45 @@
 package borg.trikeshed.parse.json
 
 // Define the Codec interface
-interface Codec<I, O> {
-    fun encode(input: Iterable<I>): Sequence<O>
-    fun decode(output: Iterable<O>): Sequence<I>
+abstract class Codec<I, O> {
+    abstract fun encode(input: Iterable<I>): Sequence<O>
+    abstract fun decode(output: Iterable<O>): Sequence<I>
+    inner class FrequencyModel {
+        private val frequencyTable = mutableMapOf<I, Int>()
+
+        fun updateFrequency(c: I) {
+
+            val currentCount = frequencyTable.getOrDefault(c, 0)
+
+        }
+
+        fun getFrequency(c: I): Int {
+            return frequencyTable.getOrDefault(c, 0)
+        }
+    }
 }
 
-// Implement the Codec for null processing
-class CounterCodec : Codec<Char, Int> {
-    val counters = mutableMapOf<Char, Int>()
+class CounterCodec : Codec<Char, Int>() {
+    private val frequencyModel = FrequencyModel()
+
     override fun encode(input: Iterable<Char>): Sequence<Int> = sequence {
-        for (c in input) { //count
-
-            (counters.getOrDefault(c, 0) + 1).also { counters[c] = it }
-
+        for (c in input) {
+            frequencyModel.updateFrequency(c)
             yield(c.code)
         }
     }
 
     override fun decode(output: Iterable<Int>): Sequence<Char> = sequence { for (c in output) yield(c.toChar()) }
+
+    fun getFrequency(c: Char): Int {
+        return frequencyModel.getFrequency(c)
+    }
 }
+
 
 fun main() {
     val codec = CounterCodec()
     val input = listOf("Codec", "example", "Iterable", "Sequence").toString()
-
 
     println("Input: $input")
 
@@ -38,4 +53,5 @@ fun main() {
 
     // Verify if input and round-trip are the same
     println("Input and round-trip are the same: ${input == roundTrip}")
+
 }
