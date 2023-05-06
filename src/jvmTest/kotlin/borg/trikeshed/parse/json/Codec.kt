20ef1a04cef181f4378cf391e3db1d64551338f4
jim <northrup.james@gmail.com>
2023-05-06 16:53:51 +0800

    Add a cumulative frequency model:
    Add a method to the FrequencyModel class to generate the cumulative frequency table.

--- src/jvmTest/kotlin/borg/trikeshed/parse/json/Codec.kt
+++ src/jvmTest/kotlin/borg/trikeshed/parse/json/Codec.kt
@@ -1,26 +1,37 @@
 package borg.trikeshed.parse.json
 
 // Define the Codec interface
-abstract class Codec<I, O> {
+abstract class Codec<I : Comparable<I>, O> {
     abstract fun encode(input: Iterable<I>): Sequence<O>
     abstract fun decode(output: Iterable<O>): Sequence<I>
     inner class FrequencyModel {
-        private val frequencyTable = mutableMapOf<I, Int>()
+        val frequencyTable = mutableMapOf<I, Int>()
+        var totalFrequency = 0
 
         fun updateFrequency(c: I) {
-
             val currentCount = frequencyTable.getOrDefault(c, 0)
-
+            frequencyTable[c] = currentCount + 1
+            totalFrequency++
         }
 
         fun getFrequency(c: I): Int {
             return frequencyTable.getOrDefault(c, 0)
         }
+
+        fun getCumulativeFrequency(c: I): Double {
+            var cumulativeFrequency = 0.0
+            for ((key, value) in frequencyTable) {
+                if (key <= c) {
+                    cumulativeFrequency += value
+                }
+            }
+            return cumulativeFrequency / totalFrequency
+        }
     }
 }
 
 class CounterCodec : Codec<Char, Int>() {
-    private val frequencyModel = FrequencyModel()
+    val frequencyModel = FrequencyModel()
 
     override fun encode(input: Iterable<Char>): Sequence<Int> = sequence {
         for (c in input) {
@@ -54,4 +65,10 @@ fun main() {
     // Verify if input and round-trip are the same
     println("Input and round-trip are the same: ${input == roundTrip}")
 
+    // Get frequency and cumulative frequency of 'e'
+    val e = 'e'
+    val frequency = codec.getFrequency(e)
+    val cumulativeFrequency = codec.frequencyModel.getCumulativeFrequency(e)
+    println("Frequency of '$e': $frequency")
+    println("Cumulative frequency of '$e': $cumulativeFrequency")
 }
