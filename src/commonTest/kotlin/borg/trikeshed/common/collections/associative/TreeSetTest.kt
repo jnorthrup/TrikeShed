afe6fe4ec528ac57d44c324847b448d7b1efc69a
jim <me@example.org>
2024-09-26 22:11:39 -0400

    wip

--- src/commonTest/kotlin/borg/trikeshed/common/collections/associative/TreeSetTest.kt
+++ src/commonTest/kotlin/borg/trikeshed/common/collections/associative/TreeSetTest.kt
@@ -2,16 +2,16 @@ package borg.trikeshed.common.collections.associative
 
 import kotlin.test.Test
 import kotlin.test.assertTrue
-class TreeSetTest{}
-@Test
-fun testTreeSet() {
-    val set = TreeSet<Int>()
-    set.add(1)
-    set.add(2)
-    set.add(3)
+class TreeSetTest {
+    @Test
+    fun testTreeSet() {
+        val set = TreeSet<Int>()
+        set.add(1)
+        set.add(2)
+        set.add(3)
 
-    assertTrue(set.contains(1))
-    assertTrue(set.contains(2))
-    assertTrue(set.contains(3))
+        assertTrue(set.contains(1))
+        assertTrue(set.contains(2))
+        assertTrue(set.contains(3))
+    }
 }
-
