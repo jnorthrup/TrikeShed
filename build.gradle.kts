fa759fb705239c0c30253dc68df6d285fe7e401d
Jim Northrup <jnorthrup@users.noreply.github.com>
2024-11-20 12:31:33 -0500

    feat: Update Kotlin Multiplatform version to 2.0.20

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,33 +1,3 @@
-<<<<<<< HEAD
-plugins {
-    kotlin("multiplatform") version "1.9.0"
-}
-
-kotlin {
-    // JVM target
-    jvm(  ) {
-        withJava()
-    }
-
-    // JS target for WebAssembly/JavaScript
-    js(IR) {
-        browser()
-        nodejs()
-    }
-
-    // Native POSIX targets
-    macosX64("macos")
-    linuxX64("linux")
-
-    sourceSets {
-        // Common code for all platforms (shared logic)
-        val commonMain by getting
-        val commonTest by getting
-
-        // POSIX-compliant code (shared between macOS and Linux)
-        val posixMain by creating {
-            dependsOn(commonMain)
-=======
 
 
 import org.jetbrains.kotlin.gradle.DeprecatedTargetPresetApi
@@ -36,7 +6,8 @@ import org.jetbrains.kotlin.gradle.ExperimentalKotlinGradlePluginApi
 //which stanza do we add a linux64 cinteropdef for liburing below? (the linux64 stanza is the only one that has a cinterop block)
 
 plugins {
-    kotlin("multiplatform") version "2.0.20" // Latest version of Kotlin Multiplatform as of now
+    kotlin("multiplatform") version "2.0.20"   // Latest version of Kotlin Multiplatform as of now
+
     `maven-publish`
 }
 
@@ -61,8 +32,8 @@ publishing {
 
 kotlin {
     @OptIn(ExperimentalKotlinGradlePluginApi::class) compilerOptions {
-        apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_1)
-        languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_1)
+        apiVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
+        languageVersion.set(org.jetbrains.kotlin.gradle.dsl.KotlinVersion.KOTLIN_2_0)
         freeCompilerArgs = listOf(
             "-opt-in=kotlin.RequiresOptIn", // Add more opt-in annotations as needed
             "-Xsuppress-version-warnings", // Suppress version warnings
@@ -87,7 +58,6 @@ kotlin {
             System.getProperty("os.arch") == "aarch64") {
             listOf(
                 macosX64("macos"),
-
                 ).first()
         } else {
             macosArm64("macos")
@@ -108,24 +78,14 @@ kotlin {
                 api("org.jetbrains.kotlinx:kotlinx-datetime:0.4.0")
                 api("org.jetbrains.kotlin:kotlin-reflect:1.8.0")
             }
->>>>>>> posixish
         }
 
-        // macOS-specific code
-        val macosMain by getting {
-            dependsOn(posixMain)
+        val commonTest by getting {
+            dependencies {
+                implementation(kotlin("test-common"))
+                implementation(kotlin("test-annotations-common"))
+            }
         }
-<<<<<<< HEAD
-
-        // Linux-specific code (liburing, etc.)
-        val linuxMain by getting {
-            dependsOn(posixMain)
-        }
-
-        // JVM-specific code
-        val jvmMain by getting {
-            dependsOn(commonMain)
-=======
         val jvmMain by getting {
             dependencies {
                 //datetime
@@ -135,17 +95,15 @@ kotlin {
                 //serialization
                 api("org.jetbrains.kotlinx:kotlinx-serialization-json-jvm:1.3.0")
             }
->>>>>>> posixish
         }
 
-        // JS-specific code
-        val jsMain by getting {
-            dependsOn(commonMain)
+        val jvmTest by getting {
+            //bring in the dependencies from jvmMain
+            dependencies {
+                implementation(kotlin("test"))
+                implementation(kotlin("test-junit"))
+            }
         }
-<<<<<<< HEAD
-    }
-}
-=======
 
 
         if (hostOs in listOf("Linux", "Mac OS X")) {
@@ -207,4 +165,3 @@ tasks.withType<org.jetbrains.kotlin.gradle.tasks.KotlinTest> {
     }
 }
 
->>>>>>> posixish
