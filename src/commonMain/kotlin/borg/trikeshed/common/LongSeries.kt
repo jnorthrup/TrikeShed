78d729b0212529d4c57ea4b17b1cc69988befdce
Jim Northrup <jnorthrup@users.noreply.github.com>
2024-11-22 20:35:58 -0500

    size, again

--- src/commonMain/kotlin/borg/trikeshed/common/LongSeries.kt
+++ src/commonMain/kotlin/borg/trikeshed/common/LongSeries.kt
@@ -4,9 +4,8 @@ import borg.trikeshed.lib.*
 
 /** Series with long Indexes for large files */
 typealias LongSeries<T> = Join<Long, (Long) -> T>
-val <V>LongSeries<V>.`▶` get() = require(  this.size < Int.MAX_VALUE ).run {  toSeries().`▶` }
 
-val <T> LongSeries<T>.size:Long get() = a
+val <V>LongSeries<V>.`▶` get() = require(this.size < Int.MAX_VALUE).run { toSeries().`▶` }
 
 operator fun <T> LongSeries<T>.get(i: Long): T = b(i)
 
@@ -24,12 +23,16 @@ fun <T> LongSeries<T>.slice(start: Long, end: Long = size): LongSeries<T> =
     (end - start) j { x -> this[start + x] }
 
 fun <T> LongSeries<T>.drop(removeInitial: Long): LongSeries<T> = slice(removeInitial)
-fun <A> Series<A>.toLongSeries():LongSeries<A>  =  a.toLong() j {it:Long -> b(it.toInt())}
+fun <A> Series<A>.toLongSeries(): LongSeries<A> = a.toLong() j { it: Long -> b(it.toInt()) }
+
 //LongSeries.toSeries
-fun <A> LongSeries<A>.toSeries(start:Long=0L, len:Int=a.toInt(). also{ require ( a < Int.MAX_VALUE ) } ):Series<A>  =  len j { it:Int -> b(start+it )}
+fun <A> LongSeries<A>.toSeries(start: Long = 0L, len: Int = a.toInt().also { require(a < Int.MAX_VALUE) }): Series<A> =
+    len j { it: Int -> b(start + it) }
 
 
-
+val <T>LongSeries<T>.size get() = a
+val <T> LongSeries<T>.isEmpty get() = size == 0L
+val <T> LongSeries<T>.isNotEmpty get() = size != 0L
 
 
 
