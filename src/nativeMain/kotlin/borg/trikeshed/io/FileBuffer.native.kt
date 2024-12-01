88ac1cadb8d951faafc49469b40386a6d3c3861d
Jim Northrup <jnorthrup@users.noreply.github.com>
2024-12-01 06:41:19 -0500

    feat: Add native implementation of FileBuffer with placeholder methods

---
+++ src/nativeMain/kotlin/borg/trikeshed/io/FileBuffer.native.kt
@@ -0,0 +1,47 @@
+package borg.trikeshed.io
+
+/**
+ * an openable and closeable mmap file.
+ *
+ *  get has no side effects but put has undefined effects on size and sync
+ */
+actual class FileBuffer actual constructor(
+    filename: String,
+    initialOffset: Long,
+    blkSize: Long,
+    readOnly: Boolean,
+) : LongSeries<Byte> {
+    actual val a: Long
+        get() = TODO("Not yet implemented")
+    actual val b: (Long) -> Byte
+        get() = TODO("Not yet implemented")
+    actual val filename: String
+        get() = TODO("Not yet implemented")
+    actual val initialOffset: Long
+        get() = TODO("Not yet implemented")
+    actual val blkSize: Long
+        get() = TODO("Not yet implemented")
+    actual val readOnly: Boolean
+        get() = TODO("Not yet implemented")
+
+    actual fun close() {
+    }
+
+    actual fun open() {
+    }
+
+    actual fun isOpen(): Boolean {
+        TODO("Not yet implemented")
+    }
+
+    actual fun size(): Long {
+        TODO("Not yet implemented")
+    }
+
+    actual fun get(index: Long): Byte {
+        TODO("Not yet implemented")
+    }
+
+    actual fun put(index: Long, value: Byte) {
+    }
+}
