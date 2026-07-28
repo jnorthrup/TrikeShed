b2a0c9d3702bc5e9f64d0937ec67bb488ddcc1e4
jnorthrup <jnorthrup@example.com>
2026-07-27 21:14:02 -0500

    flywheel: patch Implement SctpBoundedStream FIFO backpressure (296880)

--- src/commonMain/kotlin/borg/trikeshed/sctp/SctpBoundedStream.kt
+++ src/commonMain/kotlin/borg/trikeshed/sctp/SctpBoundedStream.kt
@@ -1,29 +1,19 @@
-/*
- * Copyright (C) 2024 TrikeShed
- *
- * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Affero General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This program is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Affero General Public License for more details.
- *
- * You should have received a copy of the GNU Affero General Public License
- * along with this program.  If not, see <https://www.gnu.org/licenses/>.
- */
 package borg.trikeshed.sctp
 
+import kotlinx.coroutines.channels.Channel
+
 class SctpBoundedStream(val capacity: Int = 10) {
+    init {
+        require(capacity > 0) { "Capacity must be positive" }
+    }
+
+    private val channel = Channel<ByteArray>(capacity)
 
     suspend fun enqueue(data: ByteArray) {
-        // To be implemented
+        channel.send(data)
     }
 
     suspend fun dequeue(): ByteArray {
-        // To be implemented
-        return ByteArray(0)
+        return channel.receive()
     }
 }
