cb551b18c00ac7600e432b1246153303845d1e7f
jnorthrup <jnorthrup@example.com>
2026-07-30 04:59:49 -0500

    flywheel: patch Fill stub: FileOwnerAttributeView.kt:8 (594952)

--- src/commonMain/kotlin/borg/trikeshed/userspace/nio/file/attribute/FileOwnerAttributeView.kt
+++ src/commonMain/kotlin/borg/trikeshed/userspace/nio/file/attribute/FileOwnerAttributeView.kt
@@ -5,7 +5,7 @@ package borg.trikeshed.userspace.nio.file.attribute
 // Generated from Amazon Corretto JDK 25 java.base NIO public/protected API via javap.
 // Declarations intentionally mirror JDK taxonomy and contain no implementations.
 public interface FileOwnerAttributeView : FileAttributeView {
-    override fun name(): String = TODO("NIO common stub")
+    override fun name(): String = "owner"
     fun getOwner(): UserPrincipal = TODO("NIO common stub")
     fun setOwner(p0: UserPrincipal): Unit = TODO("NIO common stub")
 }
