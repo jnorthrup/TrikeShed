fce35f66486511671ffa0bc0931f41b35001de3c
jnorthrup <jnorthrup@example.com>
2026-07-30 05:15:08 -0500

    flywheel: patch Fill stub: ConfixClassfileDir.kt:41 (919873)

--- build.gradle.kts
+++ build.gradle.kts
@@ -143,6 +143,7 @@ kotlin {
 <<<<<<< ours
 <<<<<<< ours
 <<<<<<< ours
+<<<<<<< ours
 
 =======
 >>>>>>> theirs
@@ -178,6 +179,8 @@ kotlin {
 >>>>>>> theirs
 =======
 
+>>>>>>> theirs
+=======
 >>>>>>> theirs
         macosArm64("macos") {
             compilations.getByName("main") {
