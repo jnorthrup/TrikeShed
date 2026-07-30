7653038fccf46c4ee6b3dc0fc466f061d67601f2
jnorthrup <jnorthrup@example.com>
2026-07-30 06:02:37 -0500

    flywheel: patch RGA watch service, keys, and events: 5 files, 21 c (654334)

--- src/commonMain/kotlin/borg/trikeshed/userspace/nio/file/StandardWatchEventKinds.kt
+++ src/commonMain/kotlin/borg/trikeshed/userspace/nio/file/StandardWatchEventKinds.kt
@@ -5,10 +5,15 @@ package borg.trikeshed.userspace.nio.file
 // Generated from Amazon Corretto JDK 25 java.base NIO public/protected API via javap.
 // Declarations intentionally mirror JDK taxonomy and contain no implementations.
 public class StandardWatchEventKinds {
+    private class StdWatchEventKind<T : Any>(private val _name: String, private val _type: kotlin.reflect.KClass<T>) : borg.trikeshed.userspace.nio.file.WatchEvent.Kind<T> {
+        override fun name(): String = _name
+        override fun type(): kotlin.reflect.KClass<T> = _type
+    }
+
     companion object {
-        val OVERFLOW: borg.trikeshed.userspace.nio.file.WatchEvent.Kind<Any> = TODO("NIO common stub")
-        val ENTRY_CREATE: borg.trikeshed.userspace.nio.file.WatchEvent.Kind<borg.trikeshed.userspace.nio.file.Path> = TODO("NIO common stub")
-        val ENTRY_DELETE: borg.trikeshed.userspace.nio.file.WatchEvent.Kind<borg.trikeshed.userspace.nio.file.Path> = TODO("NIO common stub")
-        val ENTRY_MODIFY: borg.trikeshed.userspace.nio.file.WatchEvent.Kind<borg.trikeshed.userspace.nio.file.Path> = TODO("NIO common stub")
+        val OVERFLOW: borg.trikeshed.userspace.nio.file.WatchEvent.Kind<Any> = StdWatchEventKind("OVERFLOW", Any::class)
+        val ENTRY_CREATE: borg.trikeshed.userspace.nio.file.WatchEvent.Kind<borg.trikeshed.userspace.nio.file.Path> = StdWatchEventKind("ENTRY_CREATE", borg.trikeshed.userspace.nio.file.Path::class)
+        val ENTRY_DELETE: borg.trikeshed.userspace.nio.file.WatchEvent.Kind<borg.trikeshed.userspace.nio.file.Path> = StdWatchEventKind("ENTRY_DELETE", borg.trikeshed.userspace.nio.file.Path::class)
+        val ENTRY_MODIFY: borg.trikeshed.userspace.nio.file.WatchEvent.Kind<borg.trikeshed.userspace.nio.file.Path> = StdWatchEventKind("ENTRY_MODIFY", borg.trikeshed.userspace.nio.file.Path::class)
     }
 }
--- src/commonMain/kotlin/borg/trikeshed/userspace/nio/file/WatchService.kt
+++ src/commonMain/kotlin/borg/trikeshed/userspace/nio/file/WatchService.kt
@@ -7,8 +7,8 @@ import borg.trikeshed.lib.Closeable
 // Generated from Amazon Corretto JDK 25 java.base NIO public/protected API via javap.
 // Declarations intentionally mirror JDK taxonomy and contain no implementations.
 public interface WatchService : Closeable {
-    override fun close(): Unit = TODO("NIO common stub")
-    fun poll(): WatchKey = TODO("NIO common stub")
-    fun poll(p0: Long, p1: kotlin.time.DurationUnit): WatchKey = TODO("NIO common stub")
-    fun take(): WatchKey = TODO("NIO common stub")
+    override fun close(): Unit
+    fun poll(): WatchKey
+    fun poll(p0: Long, p1: kotlin.time.DurationUnit): WatchKey
+    fun take(): WatchKey
 }
