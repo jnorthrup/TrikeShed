08dc554e7975662cf83a573c959a18400dc27575
jnorthrup <jnorthrup@example.com>
2026-07-30 05:01:50 -0500

    flywheel: patch Fill stub: InvalidPathException.kt:10 (150873)

--- src/commonMain/kotlin/borg/trikeshed/userspace/nio/file/InvalidPathException.kt
+++ src/commonMain/kotlin/borg/trikeshed/userspace/nio/file/InvalidPathException.kt
@@ -5,9 +5,23 @@ package borg.trikeshed.userspace.nio.file
 // Generated from Amazon Corretto JDK 25 java.base NIO public/protected API via javap.
 // Declarations intentionally mirror JDK taxonomy and contain no implementations.
 public open class InvalidPathException : IllegalArgumentException {
-    constructor(p0: String, p1: String, p2: Int) : super("$p1 at index $p2 in $p0")
-    constructor(p0: String, p1: String) : super("$p1 in $p0")
-    fun getInput(): String = TODO("NIO common stub")
-    fun getReason(): String = TODO("NIO common stub")
-    fun getIndex(): Int = TODO("NIO common stub")
+    private val input: String
+    private val reason: String
+    private val index: Int
+
+    constructor(p0: String, p1: String, p2: Int) : super("$p1 at index $p2 in $p0") {
+        this.input = p0
+        this.reason = p1
+        this.index = p2
+    }
+
+    constructor(p0: String, p1: String) : super("$p1 in $p0") {
+        this.input = p0
+        this.reason = p1
+        this.index = -1
+    }
+
+    fun getInput(): String = input
+    fun getReason(): String = reason
+    fun getIndex(): Int = index
 }
