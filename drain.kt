0a671db2c9c9f7ae69ae4e6d4e635cd859098a70
jnorthrup <jnorthrup@example.com>
2026-08-01 12:27:41 -0500

    flywheel: patch Necromance: Collapse three AppendWal impls onto on (822695)

--- drain.kt
+++ drain.kt
@@ -1,3 +1,4 @@
+<<<<<<< ours
 import borg.trikeshed.jules.JulesCause
 import borg.trikeshed.utils.kanban.JulesBoardStore
 import borg.trikeshed.utils.kanban.forForgeDir
@@ -16,3 +17,28 @@ fun main() = runBlocking {
     ))
     println(forgeDir.absolutePath)
 }
+=======
+import borg.trikeshed.utils.kanban.JulesBoardStore
+import borg.trikeshed.userspace.nio.file.spi.JvmAppendWal
+import borg.trikeshed.jules.JulesCause
+import java.io.File
+import kotlinx.coroutines.runBlocking
+
+fun main() {
+    val forgeDir = File(System.getProperty("user.home"), ".local/forge")
+    forgeDir.mkdirs()
+    val walPath = File(forgeDir, "jules-board.wal")
+    val store = JulesBoardStore(JvmAppendWal(walPath))
+
+    runBlocking {
+        store.appendWork("readme-appendwal-spi-collapse", JulesCause.WorkDrained(
+            workId = "readme-appendwal-spi-collapse",
+            sessionId = "readme-appendwal-spi-collapse",
+            commitSha = "outbox:superseded",
+            taskId = "readme-appendwal-spi-collapse",
+            at = System.currentTimeMillis()
+        ))
+    }
+    println("Appended WorkDrained for readme-appendwal-spi-collapse")
+}
+>>>>>>> theirs
