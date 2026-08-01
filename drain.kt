e299ad5e47a8bcc74b6bdd026e8c920ffc483b2a
jnorthrup <jnorthrup@example.com>
2026-08-01 12:28:01 -0500

    flywheel: resolve cumulative 1-session conflicts

--- drain.kt
+++ drain.kt
@@ -1,26 +1,8 @@
-<<<<<<< ours
+```kotlin
 import borg.trikeshed.jules.JulesCause
 import borg.trikeshed.utils.kanban.JulesBoardStore
 import borg.trikeshed.utils.kanban.forForgeDir
-import java.io.File
-import kotlinx.coroutines.runBlocking
-
-fun main() = runBlocking {
-    val forgeDir = File(System.getProperty("user.home") + "/.local/forge")
-    val store = JulesBoardStore.forForgeDir(forgeDir)
-    store.appendWork("readme-couch-head-projection-rev", JulesCause.WorkDrained(
-        workId = "readme-couch-head-projection-rev",
-        sessionId = "necromanced",
-        commitSha = "superseded-by-review",
-        taskId = "supersede-pass",
-        at = System.currentTimeMillis()
-    ))
-    println(forgeDir.absolutePath)
-}
-=======
-import borg.trikeshed.utils.kanban.JulesBoardStore
 import borg.trikeshed.userspace.nio.file.spi.JvmAppendWal
-import borg.trikeshed.jules.JulesCause
 import java.io.File
 import kotlinx.coroutines.runBlocking
 
@@ -41,4 +23,4 @@ fun main() {
     }
     println("Appended WorkDrained for readme-appendwal-spi-collapse")
 }
->>>>>>> theirs
+```
