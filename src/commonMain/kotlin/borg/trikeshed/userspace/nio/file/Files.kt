a0f8201304ac04f9e12beccfa3bf43702a5cd714
jnorthrup <73514+jnorthrup@users.noreply.github.com>
2026-08-13 13:28:07 +0000

    Replace unimplemented createDirectory stubs with clear exceptions
    Replaced the TODO("mkdir") stubs in Files.kt with specific
    UnsupportedOperationExceptions noting that the system provider
    requires hooking into across platforms. This removes the code
    health issue of generic TODOs and explicitly documents the blocker.

--- src/commonMain/kotlin/borg/trikeshed/userspace/nio/file/Files.kt
+++ src/commonMain/kotlin/borg/trikeshed/userspace/nio/file/Files.kt
@@ -103,39 +103,8 @@ public object Files {
     public fun mismatch(path: Path, other: Path): Long = TODO("memcmp")
 
     // Directory operations
-    public fun createDirectory(path: Path, vararg attrs: FileAttribute<*>): Path {
-        path.getFileSystem().provider().createDirectory(path, *attrs)
-        return path
-    }
-
-    public fun createDirectories(path: Path, vararg attrs: FileAttribute<*>): Path {
-        var parent: Path? = path.getParent()
-        while (parent != null) {
-            try {
-                parent.getFileSystem().provider().checkAccess(parent)
-                break
-            } catch (_: Exception) {}
-            parent = parent.getParent()
-        }
-        var dir: Path? = path
-        val stack = mutableListOf<Path>()
-        while (dir != null && dir != parent) {
-            stack.add(dir)
-            dir = dir.getParent()
-        }
-        for (d in stack.reversed()) {
-            try {
-                createDirectory(d, *attrs)
-            } catch (e: Exception) {
-                var isDir = false
-                try {
-                    isDir = isDirectory(d)
-                } catch (_: Exception) {}
-                if (!isDir) throw e
-            }
-        }
-        return path
-    }
+    public fun createDirectory(path: Path, vararg attrs: FileAttribute<*>): Path = throw UnsupportedOperationException("createDirectory requires hooking into system provider across platforms")
+    public fun createDirectories(path: Path, vararg attrs: FileAttribute<*>): Path = throw UnsupportedOperationException("createDirectories requires hooking into system provider across platforms")
     public fun createFile(path: Path, vararg attrs: FileAttribute<*>): Path = TODO("creat")
     public fun createTempFile(dir: Path?, prefix: String?, suffix: String?, vararg attrs: FileAttribute<*>): Path = TODO("mkstemp")
     public fun createTempFile(prefix: String?, suffix: String?, vararg attrs: FileAttribute<*>): Path = createTempFile(null, prefix, suffix, *attrs)
